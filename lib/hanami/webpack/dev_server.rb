d5c5616325267b27c87d4997ed61b8758bfe080c
Andrei Po <sickphilosopher@gmail.com>
2017-09-10 14:33:58 +0300

    added shellescape to enviroment variables

--- lib/hanami/webpack/dev_server.rb
+++ lib/hanami/webpack/dev_server.rb
@@ -1,3 +1,5 @@
+require 'shellwords'
+
 module Hanami
   module Webpack
     module DevServer
@@ -6,13 +8,18 @@ module Hanami
           envs = {
             "HANAMI_WEBPACK_DEV_SERVER_PORT" => Webpack.config.dev_server.port,
             "HANAMI_WEBPACK_DEV_SERVER_HOST" => Webpack.config.dev_server.host,
-            "HANAMI_WEBPACK_DEV_SERVER_USING" => Webpack.config.dev_server.using?.to_s,
-            "HANAMI_WEBPACK_PUBLIC_PATH" => Webpack.config.public_path
+            "HANAMI_WEBPACK_DEV_SERVER_USING" => Webpack.config.dev_server.using?,
+            "HANAMI_WEBPACK_PUBLIC_PATH" => Webpack.config.public_path.shellescape
           }
-
+          shellescape_hash(envs)
           spawn(envs, './node_modules/.bin/webpack-dev-server')
         end
         super
+      end
+
+      private
+      def shellescape_hash(hash)
+        Hash[hash.map{ |key, value| [k, Shellwords.escape(value)] }]
       end
     end
   end
