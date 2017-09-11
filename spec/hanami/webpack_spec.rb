b84ede901cbf92223b1182b12a174e619aabc553
Andrei Po <sickphilosopher@gmail.com>
2017-09-11 12:29:19 +0300

    added tests for config.dev_server.using?

--- spec/hanami/webpack_spec.rb
+++ spec/hanami/webpack_spec.rb
@@ -5,7 +5,25 @@ RSpec.describe Hanami::Webpack do
     expect(Hanami::Webpack::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  context ".config" do
+    before(:all) do
+      Hanami::Webpack.define_singleton_method :reset_config do
+        remove_instance_variable :@_config if defined? @_config
+      end
+    end
+
+    before(:each) do
+      Hanami::Webpack.reset_config
+    end
+
+    it "sets dev_server.using? to true in development env" do
+      allow(Hanami).to receive(:env).and_return("development")
+      expect(Hanami::Webpack.config.dev_server.using?).to be true
+    end
+
+    it "sets dev_server.using? to false if env != development" do
+      allow(Hanami).to receive(:env).and_return("production")
+      expect(Hanami::Webpack.config.dev_server.using?).to be false
+    end
   end
 end
