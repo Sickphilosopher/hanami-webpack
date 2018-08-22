c390cfd3522a17bba2f58f2b46acbf4539ba1bd8
Andrei Po <sickphilosopher@gmail.com>
2018-08-22 12:20:08 +0300

    added ability to override config from cli

--- lib/hanami/webpack/cli/commands/build.rb
+++ lib/hanami/webpack/cli/commands/build.rb
@@ -6,22 +6,36 @@ module Hanami
     module Cli
       module Commands
         class Build < Hanami::CLI::Commands::Command
+          WEBPACK_EXECUTABLES = %w(parallel-webpack webpack)
           requires 'finalizers'
           desc 'Build Webpack bundles'
-          WEBPACK_EXECUTABLES = %w(parallel-webpack webpack)
+          argument :config, desc: "Config overrides"
 
-          def call(*)
+          def call(config: '', **)
+            override_config(config) if config.length > 0
             exec Webpack.enviroment_variables, webpack_exe, *Webpack.webpack_cli_arguments
           end
 
-          def webpack_exe
+          private def webpack_exe
             executable = WEBPACK_EXECUTABLES
+              .lazy
               .map { |e| node_modules_exe_path(e) }
               .find { |e| File.exists?(e) }
           end
 
-          def node_modules_exe_path(exe_name)
+          private def node_modules_exe_path(exe_name)
             "./node_modules/.bin/#{exe_name}"
+          end
+
+          private def override_config(config)
+            overrides = config.split(/\s+/).map {|c| c.split(?=) }.to_h
+            return if overrides.count == 0
+
+            Webpack.configure do |configuration|
+              overrides.each do |key, value|
+                configuration.send((key + ?=).to_sym, value)
+              end
+            end
           end
         end
       end
